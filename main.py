d6e9526cd2525b94d125f03530b507a31db746d0
Pavel_Vatsek <pvatsek@gmail.com>
2023-01-29 22:33:08 +0300

    add first TG bot

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from bot_commands import *
 
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
-
-app = ApplicationBuilder().token("YOUR TOKEN HERE").build()
+app = ApplicationBuilder().token("5918976065:AAGbKQWe8gC0q-yc9YPqqmLiyN4Mk15prYc").build()
 
 app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("time", time))
+app.add_handler(CommandHandler("help", help))
+app.add_handler(CommandHandler("sum", sum))
+
+print('Server Start')
 
 app.run_polling()
